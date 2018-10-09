d7da54623c97efbfe214e72693655d2b214e6306
mitsu <mitsukudeyudai@kouraideyuudainoMacBook-Air.local>
2018-10-09 10:25:47 +0900

    create newpage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_074303) do
+ActiveRecord::Schema.define(version: 2018_10_09_012036) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_074303) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "image_name"
   end
 
 end
