cf99d9f3643085c0c820f4107c67a3bee2aab7b7
mitsu <mitsukudeyudai@kouraideyuudainoMacBook-Air.local>
2018-10-09 09:51:16 +0900

    commit on master2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
 class UsersController < ApplicationController
 
   def login
+
   end
 
   def login_form
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+        name: params[:name],
+        email: params[:email],
+        image_name: "default_user.jpg",
+        password: params[:password]
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "login" => "users#login_form"
   post "login" => "users#login"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
 end
+
