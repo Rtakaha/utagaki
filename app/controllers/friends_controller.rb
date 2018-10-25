57735730bde8fc6b43dd0a6832e32f4271171e62
mitsu <mitsukudeyudai@kouraideyuudainoMacBook-Air.local>
2018-10-25 11:34:35 +0900

    Destroy varidate

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
 
+
   def show
   end
 
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,15 @@
 class Friend < ApplicationRecord
   validates :followfrom_id, presence: true
   validates :followto_id, presence: true
+
+  before_destroy :should_not_destroy_if_presence
+
+  private
+
+  def should_not_destroy_if_presence
+    if followfrom_id.nil? || followto_id.nil?
+      throw :abort
+    end
+  end
+
 end
