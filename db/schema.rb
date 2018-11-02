eea4b80f72df0405430a76ef63d3e90dac249cba
Kenta Hayashi <kenta1989>
2018-11-02 16:24:45 +0900

    Simple_chat_room

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2018_10_23_033047) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
